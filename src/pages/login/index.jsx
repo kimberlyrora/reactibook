11eff425fbce94efeb39d4b35e7ae0daa642134f
kimberlyrojasramirez <klrojas@synopsis.ws>
2020-02-09 06:15:42 -0500

    validate input

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -9,13 +9,20 @@ const Login = () => {
 		  required 
 		  id='standard-required' 
 		  label='Usuario' 
+		  inputProps={{ 
+			// [^a-zA-Z0-9Ññ!#$%&*+/=?^_`{|}~-]
+			  pattern: '/^[a-zA-Z0-9Ññ!#$%&\'*+/=?^_`{|}~-]+(?:.[a-zA-Z0-9Ññ!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$/',
+			  whiteSpace: true,
+			  maxLength: 255
+			}}
 		  />
-		  <TextField 
-		  required 
-		  id='standard-required' 
-		  label='Password' 
-		  />
-}		</>
+		  <TextField
+          id='standard-password-input'
+          label='Password'
+          type='password'
+          autoComplete='current-password'
+        />
+	</>
 	);
 };
 
